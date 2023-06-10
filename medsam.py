fee3187ffa08c228cab83f57b84da108eba1062c
nghiemkythu <nghiemdt2013@gmail.com>
2023-06-10 14:17:27 +0700

    add medsam.py

--- medsam.py
+++ medsam.py
@@ -3,6 +3,7 @@ from segmentation_3d.MedSAM import medsam_3d
 from utils.func import (
     parse_config,
     load_config
+)
 if __name__=="__main__":
     yml_args = parse_config()
     cfg = load_config(yml_args.config)
