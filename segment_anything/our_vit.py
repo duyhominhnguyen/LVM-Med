6e3a2424726e10a02deedecb2f85cf3631e7510e
ngctnnnn <ngctnnnn@gmail.com>
2023-06-26 10:34:24 +0700

    :sparkles: vit_h for lvm-med encoder

--- segment_anything/our_vit.py
+++ segment_anything/our_vit.py
@@ -74,4 +74,31 @@ def vit_encoder_b():
         )
     return image_encoder
 
+def vit_encoder_h():
+    prompt_embed_dim = 256
+    image_size = 1024
+    vit_patch_size = 16
+    image_embedding_size = image_size // vit_patch_size
 
+
+    encoder_embed_dim=1280
+    encoder_depth=32
+    encoder_num_heads=16
+    encoder_global_attn_indexes=[7, 15, 23, 31]
+
+    image_encoder=ImageEncoderViT(
+            depth=encoder_depth,
+            embed_dim=encoder_embed_dim,
+            img_size=image_size,
+            mlp_ratio=4,
+            norm_layer=partial(torch.nn.LayerNorm, eps=1e-6),
+            num_heads=encoder_num_heads,
+            patch_size=vit_patch_size,
+            qkv_bias=True,
+            use_rel_pos=True,
+            use_abs_pos = False,
+            global_attn_indexes=encoder_global_attn_indexes,
+            window_size=14,
+            out_chans=prompt_embed_dim,
+        )
+    return image_encoder
