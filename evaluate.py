f54727043b1fa127cdb5d4cca2f3e62172a477c2
nghiemkythu <nghiemdt2013@gmail.com>
2023-07-12 21:27:04 +0700

    add 3D dice score in evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -111,3 +111,26 @@ def evaluate_3d_iou_fast(net, dataset, device, eval_class):
             iou_score += multiclass_iou(mask_pred_vector[:, eval_class, ...], mask_true_vector[:, eval_class, ...], reduce_batch_first=True)
     net.train()
     return iou_score/num_items
+
+def evaluate_3d_dice(net, dataset, device, eval_class):
+    net.eval()
+    dice_score = 0
+    # iterate over the validation set
+    num_items = 0
+    for image_3d in tqdm(dataset.get_3d_iter(), desc='3D Evaluation', unit='image(s)', leave=False):
+        image, mask_true = image_3d['image'], image_3d['mask_ete']
+        num_items += 1
+        # move images and labels to correct device and type
+
+        image = image.to(device=device, dtype=torch.float32)
+        mask_true = mask_true.to(device=device, dtype=torch.long)
+        mask_true_vector = F.one_hot(mask_true, num_classes).permute(0, 3, 1, 2).float()
+
+        with torch.no_grad():
+            # predict the mask
+            mask_pred = net(image)
+            mask_pred = mask_pred.argmax(dim=1)
+            mask_pred_vector = F.one_hot(mask_pred, num_classes).permute(0, 3, 1, 2).float()
+            dice_score += multiclass_dice_coeff(mask_pred_vector[:, eval_class, ...], mask_true_vector[:, eval_class, ...], reduce_batch_first=True)
+    net.train()
+    return dice_score/num_items
